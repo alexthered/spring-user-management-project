4921ea0d9607351a87cb68c18b13ad152a536a6a
Hiep Doan <hiep.dv179@gmail.com>
2017-03-31 15:15:47 +0200

    Initial commit

---
+++ src/main/java/com/alexthered/me/SpringUserManagementProjectApplication.java
@@ -0,0 +1,12 @@
+package com.alexthered.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserManagementProjectApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(SpringUserManagementProjectApplication.class, args);
+	}
+}
