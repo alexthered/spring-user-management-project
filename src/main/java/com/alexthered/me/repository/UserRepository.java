ab4ab0faa0b116fe9fa1ad28987b344a243c51aa
Hiep Doan <hiep.dv179@gmail.com>
2017-04-04 09:02:18 +0200

    add Repository annotation for UserRepository class

--- src/main/java/com/alexthered/me/repository/UserRepository.java
+++ src/main/java/com/alexthered/me/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.alexthered.me.repository;
 
 import com.alexthered.me.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by hd on 03.04.17.
  */
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
